77f09111d3b24532b7074eaf510c50bb70ad973d
Lucypigr <156533398+Lucypigr@users.noreply.github.com>
2026-09-13 01:55:51 +0800

    fix: enable upright 2.5D renderer

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { Game } from './game/Game.js';
-import { enableIsoMode } from './game/IsoMode.js';
+import { enableIsoMode2 } from './game/IsoMode2.js';
 
 const canvas=document.querySelector('#game');
 const game=new Game(canvas);
-enableIsoMode(game);
+enableIsoMode2(game);
