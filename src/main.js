48b4f31e541aaae437d5e331112f9cb0c0659bd5
Lucypigr <156533398+Lucypigr@users.noreply.github.com>
2026-09-13 02:03:32 +0800

    fix: enable upright billboard sprite renderer

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { Game } from './game/Game.js';
 import { enableIsoMode2 } from './game/IsoMode2.js';
+import { enableUprightSprites } from './game/UprightSprites.js';
 
 const canvas=document.querySelector('#game');
 const game=new Game(canvas);
 enableIsoMode2(game);
+enableUprightSprites(game);
