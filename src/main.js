c54427337df7f433893ae118a13e8baca61f9a57
Lucypigr <156533398+Lucypigr@users.noreply.github.com>
2026-09-13 12:16:03 +0800

    feat: enable linked gem gameplay

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { Game } from './game/Game.js';
 import { enableIsoMode2 } from './game/IsoMode2.js';
 import { enableUprightSprites } from './game/UprightSprites.js';
+import { enableGemSystem } from './game/systems/GemSystem.js';
 
 const canvas=document.querySelector('#game');
 const game=new Game(canvas);
 enableIsoMode2(game);
 enableUprightSprites(game);
+enableGemSystem(game);
